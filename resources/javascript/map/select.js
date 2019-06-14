13bf50f502300187ca0a4f649a6da7f87e74f226
Jonathan Beliën <jbe@geo6.be>
2019-06-14 14:12:43 +0200

    Update select.js

--- resources/javascript/map/select.js
+++ resources/javascript/map/select.js
@@ -5,6 +5,41 @@ import Collection from 'ol/Collection';
 
 import displayRecord from './feature/display';
 
+function onselect (event, features) {
+    window.app.highlightLayer.getSource().clear();
+
+    const collection = new Collection();
+
+    features.forEach(feature => {
+        const properties = feature.getProperties();
+
+        if (typeof properties.features !== 'undefined') {
+            // Cluster
+            collection.extend(properties.features);
+        } else {
+            // Feature
+            collection.push(feature);
+        }
+    });
+
+    const liElement = Array.prototype.filter.call(
+        document.querySelectorAll('.sidebar-tabs > ul > li'),
+        liElement => liElement.querySelector('a[href="#info"]') !== null
+    )[0];
+
+    if (collection.getLength() > 0) {
+        liElement.classList.remove('disabled');
+
+        displayRecord(collection, 0);
+
+        window.app.sidebar.open('info');
+    } else {
+        liElement.classList.add('disabled');
+
+        window.app.sidebar.close();
+    }
+}
+
 export default function (map, layer) {
     const select = new Select({
         layers: [layer],
@@ -13,40 +48,7 @@ export default function (map, layer) {
         wrapX: false
     });
 
-    select.on('select', event => {
-        window.app.highlightLayer.getSource().clear();
-
-        const features = new Collection();
-
-        select.getFeatures().forEach(feature => {
-            const properties = feature.getProperties();
-
-            if (typeof properties.features !== 'undefined') {
-                // Cluster
-                features.extend(properties.features);
-            } else {
-                // Feature
-                features.push(feature);
-            }
-        });
-
-        const liElement = Array.prototype.filter.call(
-            document.querySelectorAll('.sidebar-tabs > ul > li'),
-            liElement => liElement.querySelector('a[href="#info"]') !== null
-        )[0];
-
-        if (features.getLength() > 0) {
-            liElement.classList.remove('disabled');
-
-            displayRecord(features, 0);
-
-            window.app.sidebar.open('info');
-        } else {
-            liElement.classList.add('disabled');
-
-            window.app.sidebar.close();
-        }
-    });
+    select.on('select', event => onselect(event, select.getFeatures()));
 
     map.addInteraction(select);
 
