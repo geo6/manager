b366cfe593f507b1184a1158d9f06f158d140ea3
Jonathan Beliën <jbe@geo6.be>
2019-06-28 15:33:50 +0200

    Add window.app to table template

--- resources/javascript/table.js
+++ resources/javascript/table.js
@@ -2,8 +2,30 @@
 
 import '../sass/table.scss';
 
+import Cache from './cache';
 import initFilter from './filter/init';
 
+window.app = {
+    cache: null,
+    custom: null,
+    thematic: null
+};
+
 (function () {
+    window.app.custom = window.custom || 'default';
+    delete window.custom;
+
+    window.app.thematic = window.thematic;
+    delete window.thematic;
+
+    window.app.cache = new Cache();
+
+    document
+        .getElementById('table-wrapper')
+        .addEventListener('scroll', function () {
+            this.querySelector('thead').style.transform =
+                'translateY(' + (this.scrollTop - 1) + 'px)';
+        });
+
     initFilter();
 })();
