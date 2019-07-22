d96b183c4f8097c6984ec3d234a34836a79619b1
jbelien <dev@jbelien.be>
2019-07-22 14:54:39 +0000

    Apply fixes from StyleCI

--- src/App/Handler/TableHandler.php
+++ src/App/Handler/TableHandler.php
@@ -14,7 +14,6 @@ use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 use Zend\Diactoros\Response\HtmlResponse;
 use Zend\Diactoros\Response\RedirectResponse;
-use Zend\Expressive\Router\RouteResult;
 use Zend\Expressive\Router\RouterInterface;
 use Zend\Expressive\Template\TemplateRendererInterface;
 
