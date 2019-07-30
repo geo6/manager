00b762bc0c6168890f995782704e490a5123ada8
Jonathan Beliën <jbe@geo6.be>
2019-07-30 15:10:15 +0200

    Update MainTable.php
    Move toArray() function to MainTable model.

--- src/App/Model/MainTable.php
+++ src/App/Model/MainTable.php
@@ -39,4 +39,44 @@ class MainTable extends Table
 
         return false;
     }
+
+    public function toArray(): array
+    {
+        $columns = [];
+        foreach ($this->columns as $column) {
+            $columns[] = [
+                'name'      => $column->getName(),
+                'type'      => $column->getDataType(),
+                'default'   => $column->getColumnDefault(),
+                'maxlength' => $column->getCharacterMaximumLength(),
+                'readonly'  => $column->readonly,
+                'notnull'   => !$column->isNullable(),
+            ];
+        }
+
+        $constraints = [];
+        foreach ($this->constraints as $constraint) {
+            $constraints[] = [
+                'name'      => $constraint->getName(),
+                'type'      => $constraint->getType(),
+                'columns'   => $constraint->hasColumns() ? $constraint->getColumns() : null,
+                'check'     => $constraint->isCheck() ? $constraint->getCheckClause() : null,
+                'reference' => $constraint->isForeignKey() ? [
+                    'schema'  => $constraint->getReferencedTableSchema(),
+                    'table'   => $constraint->getReferencedTableName(),
+                    'columns' => $constraint->getReferencedColumns(),
+                ] : null,
+            ];
+        }
+
+        return [
+            'schema' => $this->schema,
+            'table'  => $this->name,
+            // 'count' => $count,
+            'key'         => $this->getKeyColumn()->getName(),
+            'geometry'    => $this->getGeometryColumn()->getName(),
+            'columns'     => $columns,
+            'constraints' => $constraints,
+        ];
+    }
 }
