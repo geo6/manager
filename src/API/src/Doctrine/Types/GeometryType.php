7fffe7d69dde401eed5519fe6a7c5c09a2919b7d
Jonathan Beliën <dev@jbelien.be>
2021-03-17 15:56:59 +0100

    Create Doctrine GeometryType

---
+++ src/API/src/Doctrine/Types/GeometryType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace API\Doctrine\Types;
+
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\Type;
+
+class GeometryType extends Type
+{
+    const TYPE = 'geometry';
+
+    public function getSqlDeclaration(array $fieldDeclaration, AbstractPlatform $platform)
+    {
+        return self::TYPE;
+    }
+
+    public function getName()
+    {
+        return self::TYPE;
+    }
+
+    public function canRequireSQLConversion()
+    {
+        return true;
+    }
+
+    public function convertToDatabaseValueSQL($sqlExpr, AbstractPlatform $platform)
+    {
+        return sprintf('ST_GeomFromGeoJSON(%s)', $sqlExpr);
+    }
+
+    public function convertToPHPValueSQL($sqlExpr, $platform)
+    {
+        return sprintf('ST_AsGeoJSON(%s)', $sqlExpr);
+    }
+}
