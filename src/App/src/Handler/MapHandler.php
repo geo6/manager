65bc84d0924811838f93dd8ee3212fb50a216cc6
Jonathan Beliën <dev@jbelien.be>
2021-04-06 17:56:22 +0200

    Update MapHandler.php

--- src/App/src/Handler/MapHandler.php
+++ src/App/src/Handler/MapHandler.php
@@ -6,6 +6,7 @@ namespace App\Handler;
 
 use API\Middleware\DatabaseMiddleware;
 use API\Middleware\TableMiddleware;
+use Doctrine\DBAL\Schema\Column;
 use Laminas\Diactoros\Response\HtmlResponse;
 use Mezzio\Template\TemplateRendererInterface;
 use Psr\Http\Message\ResponseInterface;
@@ -29,11 +30,25 @@ class MapHandler implements RequestHandlerInterface
         /** @var Connection */ $connection = $request->getAttribute(DatabaseMiddleware::CONNECTION_ATTRIBUTE);
 
         /** @var Table */ $table = $request->getAttribute(TableMiddleware::TABLE_ATTRIBUTE);
+        /** @var string */ $primaryKey = $request->getAttribute(TableMiddleware::PRIMARYKEY_ATTRIBUTE);
+        /** @var array */ $readonlyColumns = $request->getAttribute(TableMiddleware::READONLY_ATTRIBUTE);
 
         return new HtmlResponse($this->renderer->render(
             'app::map',
             [
                 'table' => $table,
+                'readonly'    => [
+                    $primaryKey,
+                    ...$readonlyColumns,
+                    ...array_values(array_map(
+                        function (Column $column) {
+                            return $column->getName();
+                        },
+                        array_filter($table->getColumns(), function (Column $column) {
+                            return in_array($column->getType()->getName(), ['geometry', 'geography']);
+                        })
+                    )),
+                ],
             ]
         ));
     }
