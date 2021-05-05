f977d1fe5e8abb38cccf89805857df84c96c8073
Jonathan Beliën <dev@jbelien.be>
2021-05-05 16:19:15 +0200

    Update TableHandler.php

--- src/App/src/Handler/TableHandler.php
+++ src/App/src/Handler/TableHandler.php
@@ -29,18 +29,29 @@ class TableHandler implements RequestHandlerInterface
 
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
-/** @var Connection */ $connection = $request->getAttribute(DatabaseMiddleware::CONNECTION_ATTRIBUTE);
+        /** @var Connection */
+        $connection = $request->getAttribute(DatabaseMiddleware::CONNECTION_ATTRIBUTE);
 
-/** @var Table */ $table = $request->getAttribute(TableMiddleware::TABLE_ATTRIBUTE);
-/** @var bool */ $isView = $request->getAttribute(TableMiddleware::ISVIEW_ATTRIBUTE);
-/** @var string */ $primaryKey = $request->getAttribute(TableMiddleware::PRIMARYKEY_ATTRIBUTE);
-/** @var array */ $foreignKeys = $request->getAttribute(TableMiddleware::FOREIGNKEYS_ATTRIBUTE);
-/** @var int */ $count = $request->getAttribute(TableMiddleware::COUNT_ATTRIBUTE);
+        /** @var Table */
+        $table = $request->getAttribute(TableMiddleware::TABLE_ATTRIBUTE);
+        /** @var bool */
+        $isView = $request->getAttribute(TableMiddleware::ISVIEW_ATTRIBUTE);
+        /** @var string */
+        $primaryKey = $request->getAttribute(TableMiddleware::PRIMARYKEY_ATTRIBUTE);
+        /** @var array */
+        $foreignKeys = $request->getAttribute(TableMiddleware::FOREIGNKEYS_ATTRIBUTE);
+        /** @var int */
+        $count = $request->getAttribute(TableMiddleware::COUNT_ATTRIBUTE);
 
-/** @var int */ $limit = $request->getAttribute(TableMiddleware::LIMIT_ATTRIBUTE);
-/** @var array */ $readonlyColumns = $request->getAttribute(TableMiddleware::READONLY_ATTRIBUTE);
+        /** @var int */
+        $limit = $request->getAttribute(TableMiddleware::LIMIT_ATTRIBUTE);
+        /** @var array */
+        $readonlyColumns = $request->getAttribute(TableMiddleware::READONLY_ATTRIBUTE);
+        /** @var array */
+        $fileColumns = $request->getAttribute(TableMiddleware::FILE_ATTRIBUTE);
 
-/** @var int */ $offset = $request->getAttribute('offset', 0);
+        /** @var int */
+        $offset = $request->getAttribute('offset', 0);
         $offset = intval(floor(intval($offset) / $limit) * $limit);
 
         $params = $request->getQueryParams();
@@ -91,7 +102,7 @@ class TableHandler implements RequestHandlerInterface
                 ->addSelect(...$foreignSelect);
         }
 
-        $stmt = $query->execute();
+        $stmt = $query->executeQuery();
         $records = $stmt->fetchAllAssociative();
 
         return new HtmlResponse($this->renderer->render(
@@ -107,6 +118,7 @@ class TableHandler implements RequestHandlerInterface
                 'offset'      => $offset,
                 'limit'       => $limit,
                 'records'     => $records,
+                'file'        => $fileColumns,
                 'readonly'    => [
                     $primaryKey,
                     ...$readonlyColumns,
