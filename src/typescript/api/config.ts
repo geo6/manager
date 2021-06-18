12ab9586aa28a9928c4aadd14ab2b07761a5e9c1
Jonathan Beliën <dev@jbelien.be>
2021-06-18 14:23:26 +0200

    Create API config.ts

---
+++ src/typescript/api/config.ts
@@ -0,0 +1,17 @@
+export namespace API {
+  export class Config {
+    static async get (key?: string): Promise<any> {
+      console.log(key);
+
+      const response = await fetch(typeof key === 'undefined' ? '/api/config' : `/api/config/${key}`);
+
+      if (response.ok !== true) {
+        throw response.statusText;
+      }
+
+      const json = await response.json();
+
+      return json;
+    }
+  }
+}
