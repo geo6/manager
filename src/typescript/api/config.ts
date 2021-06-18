649dfaec590f4fcd81785c52750c93d741d22f9d
Jonathan Beliën <dev@jbelien.be>
2021-06-18 16:14:50 +0200

    🔇 Remove console.log

--- src/typescript/api/config.ts
+++ src/typescript/api/config.ts
@@ -1,8 +1,6 @@
 export namespace API {
   export class Config {
     static async get (key?: string): Promise<any> {
-      console.log(key);
-
       const response = await fetch(typeof key === 'undefined' ? '/api/config' : `/api/config/${key}`);
 
       if (response.ok !== true) {
